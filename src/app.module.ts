f0ce5a0965fe0786e07f9bedcee7f1ee73c57d14
RitoTorri <cortezfrancisco025gmail.com>
2026-02-15 16:59:30 -0400

    Funcion con la master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Cargar variables de entorno (.env)
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que config sea accesible en toda la app
+    }),
+
+    // 2. Usar forRootAsync para cargar la configuración mediante la clase
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
