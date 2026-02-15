a4c7ec0d5bdba72f0d6d6737ed194a1f3bf2cc53
RitoTorri <cortezfrancisco025gmail.com>
2026-02-15 16:58:29 -0400

    Se descargo la libreria de class validator y transformer. Ademas, de implementar CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
